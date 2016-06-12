33de618c3fd23fc8d941fba3fa59ab4597180a36
Claudia Carpenter <claudia.carpenter@gmail.com>
2016-06-12 09:54:34 -0700

    Adding zipcode

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ var savedToday = null;
 var savedForecast = null;
 var savedTime = null;
 var savedSunset = null;
+var savedLocation = "94022";
+var savedCity = "Los Altos";
 
 function getAndUpdateWeather() {
     var yesterdayRequest = "https://api.wunderground.com/api/63291acfffacc47e/yesterday/q/CA/94022.json";
     var todayRequest = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20" +
-        "where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%2294022%22)" +
+        "where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + savedLocation + "%22)" +
         "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
     try {
@@ -33,6 +35,8 @@ function getAndUpdateWeather() {
                 savedSunset = todayData.query.results.channel.astronomy.sunset;
                 savedTime = new Date(todayData.query.results.channel.lastBuildDate);
                 savedForecast = todayData.query.results.channel.item.forecast;
+                savedCity = todayData.query.results.channel.location.city + ',' +
+                    todayData.query.results.channel.location.region;
                 displayWeather();
             }});
     } catch (ex) {
@@ -41,16 +45,16 @@ function getAndUpdateWeather() {
 }
 
 function displayWeather() {
-    if (savedToday != null) {
+    if (savedToday) {
         $('#weatherIcon').attr('class', "wi wi-" + yahooCodes[savedToday.code]); //attr('src', subDir + savedToday.code + '.png');
         $('#weatherTemp').html(savedToday.temp + '&deg; F');
     }
 
-    if (savedYesterday != null) {
+    if (savedYesterday) {
         $('#weathery').html('Yesterday<br>' + formatTempRange(savedYesterday));
     }
 
-    if (savedForecast != null) {
+    if (savedForecast) {
         $('#weather0').html('Today<br>' + formatTempRange(savedForecast[0]) +
             '<br><i class="wi wi-' + yahooCodes[savedForecast[0].code] + '"></i>');
         $('#weather1').html('Tomorrow<br>' + formatTempRange(savedForecast[1]) +
@@ -59,12 +63,16 @@ function displayWeather() {
             '<br><i class="wi wi-' + yahooCodes[savedForecast[2].code] + '"></i>');
     }
 
-    if (savedTime != null) {
+    if (savedTime) {
         showTime();
     }
 
-    if (savedSunset != null) {
+    if (savedSunset) {
        $('#sunset').html('If all goes according to plan, the sun should set tonight @ ' + savedSunset);
+    }
+
+    if (savedCity) {
+        $('#city').html(savedCity);
     }
 }
 
@@ -101,6 +109,21 @@ function formatDate(date) {
     var days = new Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
     var months = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
     return days[date.getDay()].toUpperCase() + '&nbsp;' + (date.getMonth() + 1) + '/' + date.getDate();
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var checkLocation = getParameterByName("zipcode");
+if (checkLocation) {
+    savedLocation = checkLocation;
 }
 
 setTimeout(getAndUpdateWeather, 100);
